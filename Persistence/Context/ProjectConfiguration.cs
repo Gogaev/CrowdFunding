924d940d83816f2ea0e60c5e7ec1c864cb0743fe
Heorhii Yefremov <hy@leobit.com>
2023-10-09 16:18:06 +0300

    Adding Projects and Tiers Configuration

---
+++ Persistence/Context/ProjectConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Domain.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Context
+{
+    internal class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.ToTable("Projects");
+
+            builder.HasMany(p => p.Tiers)
+                .WithOne(t => t.Project)
+                .HasForeignKey(fk => fk.ProjectId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(p => p.Title)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(p => p.Status)
+                .IsRequired();
+
+            builder.Property(p => p.LastDay)
+                .HasColumnType("datetime2")
+                .IsRequired();
+
+            builder.Property(p => p.StartingDay)
+                .HasColumnType("datetime2")
+                .IsRequired();
+
+            builder.Property(p => p.RequiredMoney)
+                .HasPrecision(15,2)
+                .IsRequired();
+
+            builder.Property(p => p.InvestedMoney)
+                .HasPrecision(15, 2);
+        }
+    }
+}
---
+++ Persistence/Context/TierConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Domain.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Context
+{
+    internal class TierConfiguration : IEntityTypeConfiguration<Tier>
+    {
+        public void Configure(EntityTypeBuilder<Tier> builder)
+        {
+            builder.ToTable("Tiers");
+
+            builder.Property(p => p.TierName)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(p => p.RequiredMoney)
+                .HasPrecision(15, 2)
+                .IsRequired();
+
+            builder.Property(p => p.Benefit)
+                .HasMaxLength(30);
+
+            builder.Property(p => p.IsReached)
+                .IsRequired();
+        }
+    }
+}
